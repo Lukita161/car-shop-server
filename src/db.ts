86598f3e1dba581220104d70bf95976152ea9924
Luca Seara <caularase@gmail.com>
2024-11-10 14:20:38 -0300

    fixed

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-const url = 'mongodb+srv://caularase:MWiqORk6xElBh14M@car-manager-api.wga4b.mongodb.net/?retryWrites=true&w=majority&appName=car-manager-api'
+
 export default async function connectDb() {
     try {
-        await mongoose.connect(url)
+        await mongoose.connect(process.env.MOONGOSE_CONNECTION)
         console.log('Connection succesfull')
     } catch (error) {
         console.log(error)
